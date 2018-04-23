a656f3f78a013f6b83a70dd34d7fae3492f3c580
Anton Nguyen <afnguyen85@gmail.com>
2018-04-23 12:13:41 +0000

    Introduced 'test settings' and encourage the usage of the PULLSBURY_SETTINGS env variable
    Makes it easier to run your unit tests, without effecting real credentials

--- tests/test_config.py
+++ tests/test_config.py
@@ -1,11 +1,30 @@
-from unittest import TestCase
+import os
 from pullsbury.config import load_config
-from nose.tools import eq_, ok_
+from nose.tools import eq_, ok_, raises, assert_raises
 
 
 def test_load_config():
     res = load_config()
     assert res['GITHUB_USER'].endswith, 'Exists and is stringy'
+
+
+@raises(IOError)
+def test_load_config_returns_io_error_if_setting_file_is_not_found():
+    load_config(settings_file='./unknown.py')
+
+
+def test_load_config_prioritizes_the_pullsbury_settings_env_variable():
+    old_settings = os.environ.get('PULLSBURY_SETTINGS')
+    os.environ['PULLSBURY_SETTINGS'] = './unknown.py'
+
+    with assert_raises(IOError):
+        load_config()
+
+    if old_settings:
+        os.environ['PULLSBURY_SETTINGS'] = old_settings
+    else:
+        del os.environ['PULLSBURY_SETTINGS']
+
 
 def test_load_config_handles_happy_slack_emojis():
     expected_emojis = [
@@ -29,6 +48,7 @@ def test_load_config_handles_happy_slack_emojis():
     ok_(len(actual_emojis) > 0)
     eq_(actual_emojis, expected_emojis)
 
+
 def test_load_config_handles_teams():
     expected_teams = {
         "Channel": {
@@ -41,6 +61,7 @@ def test_load_config_handles_teams():
     res = load_config()
     actual_teams = res.get('TEAMS')
     eq_(actual_teams, expected_teams)
+
 
 def test_load_config_repo_blacklist():
     expected_repos = [
